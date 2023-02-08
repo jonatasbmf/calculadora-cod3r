c3839a027d4174f51231e8d463da99a8e6ff1b14
Jonatas Batista Mendonca Freitas <jonatas.freitas@localiza.com>
2023-02-08 15:31:38 -0300

    Finalizada calculadora

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import './Display.css';
 
-export default props =>
-    <div className="display">{props.value}</div>
+function Display(props) {
+    return (
+        <div className="display">
+            {props.value}
+        </div>
+    )
+}
+
+export default Display
